df7541c0d477535950a52efcbc8edb736de2cf20
Valentin Mamedov <45292985+Inf1delis@users.noreply.github.com>
2020-05-22 01:30:20 +0800

    cleaning mongo.py

--- mongo.py
+++ mongo.py
@@ -2,12 +2,6 @@ import pymongo
 
 
 def connect_to_mongo():
-    MONGO_INITDB_ROOT_USERNAME = "user"
-    MONGO_INITDB_ROOT_PASSWORD = "user"
-    host = "127.0.0.1"
-    ports = "27017"
-    # uri = "mongodb://{:s}:{:s}@{:s}:{:s}".format(MONGO_INITDB_ROOT_USERNAME, \
-    #                                              MONGO_INITDB_ROOT_PASSWORD, host, ports)
     uri = 'mongodb://127.0.0.1:27017/'
     return pymongo.MongoClient(uri)['vimpel38']
 
